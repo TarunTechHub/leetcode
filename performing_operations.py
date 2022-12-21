da4bcbdb2cd38985c88b349dc01dd2511d97e744
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-12-21 14:30:13 +0530

    initial commit

---
+++ performing_operations.py
@@ -0,0 +1,10 @@
+#	2011 Final Value of Variable After Performing Operations
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        X = 0
+        for op in operations:
+            if '+' in op:
+                X += 1
+            else:
+                X -= 1
+        return X
