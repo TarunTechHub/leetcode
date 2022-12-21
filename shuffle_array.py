1b40d1047f9e9c87f94d4007e5bc22277a7a483a
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-12-21 15:13:12 +0530

    initial commit

---
+++ shuffle_array.py
@@ -0,0 +1,10 @@
+#1470. Shuffle the Array
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[n+i])
+        return res
+Solution()
