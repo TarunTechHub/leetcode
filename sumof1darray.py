2fdeb1d34260aa08cc3518810fa8f921e16bc119
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-12-21 13:52:37 +0530

    initial commit

---
+++ sumof1darray.py
@@ -0,0 +1,10 @@
+#Running Sum of 1d Array
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        empty_array = [0] * len(nums)
+        empty_array[0] = nums[0]
+        for i in range(1,len(nums)):
+            empty_array[i] = empty_array[i-1] + nums[i]
+        return empty_array
+Solution()
