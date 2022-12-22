fa3e393715303855bc5371909812bd28c5e806dc
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-12-22 13:16:41 +0530

    initial commit

---
+++ good_pairs.py
@@ -0,0 +1,12 @@
+#1512. Number of Good Pairs
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] == nums[j]:
+                    count += 1
+        return count
+Solution()
