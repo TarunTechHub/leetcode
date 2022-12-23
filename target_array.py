47c8f0c093182f406d387feeece97a97bb59441e
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-12-23 23:03:26 +0530

    initial commit

---
+++ target_array.py
@@ -0,0 +1,10 @@
+#1389. Create Target Array in the Given Order
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target =[]
+        for i in range(len(nums)):
+            if index[i] == len(target) :
+                target.append(nums[i])
+            else:
+                target = target[:index[i]] + [nums[i]] + target[index[i]:]
+        return target
