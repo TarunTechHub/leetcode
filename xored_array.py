1c0b297dbe409a48674636df31527821230366fc
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-12-23 23:12:17 +0530

    initial commit

---
+++ xored_array.py
@@ -0,0 +1,7 @@
+#1720. Decode XORed Array
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        arr = [first] 
+        for i in encoded:
+            arr.append(arr[-1]^i)
+        return arr
